88e04f7c9570f5a8b599e11a8298984a814156e4
ashx004 <ashx004@gmail.com>
2026-06-29 21:47:09 -0500

    Placeholder component for testing

---
+++ src/components/ClassCard.tsx
@@ -0,0 +1,8 @@
+export default function ClassCard() {
+    return (
+        <div className="class-card">
+            <h3>Class Name</h3>
+            <p>Class Description</p>
+        </div>
+    );
+}
