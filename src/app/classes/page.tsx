4eeffe2d1b4cd9fbdd387beb0067bab4a5db4615
ashx004 <ashx004@gmail.com>
2026-07-02 16:48:06 -0500

    Added query functions to grab single and all enrollments

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,18 +1,88 @@
-"use client";
-
 import ClassCard, { ClassCardProps } from '@/src/components/ClassCard';
+import { doc, getDoc, collection, getDocs } from 'firebase/firestore';
+import { db } from '@/src/library/firebase';
 
-export default function Classes() {
+// database fields
+export interface ClassProps {
+    className: string;
+    classCode: string;
+    term: string;
+    time?: string;
+    facultyPhoneNumber?: string;
+    facultyOfficeNumber?: string;
+    facultyEmail?: string;
+    facultyName?: string;
+    classSchedule?: string;
+}
+
+async function getEnrollment(
+    userId: string,
+    enrollmentId: string
+): Promise<ClassProps | null> {
+    const docRef = doc(db, "users", userId, "enrollment", enrollmentId);
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) {
+        console.log("No such document!");
+        return null;
+    }
+
+    const data = docSnap.data();
+    
+    return {
+        className: data.className,
+        classCode: data.classCode,
+        term: data.term,
+        time: data.time,
+        facultyPhoneNumber: data.facultyPhoneNumber,
+        facultyOfficeNumber: data.facultyOfficeNumber,
+        facultyEmail: data.facultyEmail,
+        facultyName: data.facultyName,
+        classSchedule: data.classSchedule
+    }
+}
+
+async function getAllEnrollments(userId: string): Promise<ClassCardProps[]> {
+    const enrollmentRef = collection(db, "users", userId, "enrollment");
+    const querySnapshot = await getDocs(enrollmentRef);
+    let enrollments: ClassCardProps[] = [];
+
+    querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        enrollments.push({
+            classId: doc.id,
+            className: data.className,
+            classCode: data.classCode,
+            term: data.term,
+        });
+    });
+
+    return enrollments;
+}
+
+export default async function Classes() {
+
+    const enrollments: ClassCardProps[] = await getAllEnrollments("12345678").then((enrollments) => {
+        return enrollments;
+    }).catch((error) => {
+        console.error("Error getting enrollments: ", error);
+        return [];
+    });
+
+    enrollments.forEach((enrollment) => {
+        console.log(enrollment);
+    });
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold mb-8">Classes</h1>
-            <div className="flex flex-wrap justify-center gap-8">
-                <ClassCard courseName="Math 101" courseCode="MATH 101" term="Fall 2023" />
-                <ClassCard courseName="History 201" courseCode="HIST 201" term="Spring 2024" />
-                <ClassCard courseName="Science 301" courseCode="SCI 301" term="Summer 2023" />
-                <ClassCard courseName="Art 401" courseCode="ART 401" term="Fall 2023" />
-                <ClassCard courseName="Computer Science 501" courseCode="CS 501" term="Spring 2024" />
-                <ClassCard courseName="Literature 601" courseCode="LIT 601" term="Summer 2023" />
+        <div className="flex min-h-screen items-center justify-center 
+                    bg-orange-50">
+            <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                <h1 className="text-4xl font-bold mb-8">Classes</h1>
+                <div className="flex flex-wrap justify-center gap-8">
+                    {enrollments.map((enrollment) => (
+                        <ClassCard key={enrollment.classId} {...enrollment} />
+                    ))}
+                </div>
             </div>
         </div>
     );
