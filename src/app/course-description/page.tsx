09b0804ee7e642ceefce9eb967139f2cb0c8257e
CameronStorer <cameronstorer0@gmail.com>
2026-07-01 20:31:24 -0500

    structural improvements

--- src/app/course-description/page.tsx
+++ src/app/course-description/page.tsx
@@ -2,65 +2,79 @@ export default function Home() {
   const greeting: string = "Course Description";
 
   return (
-    // main box
-    <div className="min-h-screen justify-center pt-40  m-8 text-center bg-blue-600">
-        <h1 className="text-4xl font-bold text-black font-sans mb-2">
-          {greeting}
-        </h1>
+    // Background
+    <body className="justify-center text-center bg-gray-200">
+        {/* Inner page */}
+        <div className="rounded-2xl m-8 shadow-2xl py-20 bg-gray-400">
+            {/* Greeting */}
+            <h1 className="text-4xl font-bold text-black font-sans mb-2">
+            {greeting}
+            </h1>
 
-        <div className="m-auto flex justify-center w-[50%]">
-            <div>
-                <b>Course Name:</b><br/>
-                CSC 4052<br/>
-                
-            </div>
-            <div>
-                <b>Instructor:</b><br/>
-                Kyle Prather <br/>
-                
-            </div>
-            <div>
-                <b>Instructor office hours:</b><br/>
-                MWF 9 am - 4 pm <br/>
-                
-            </div>
-            <div>
-                <b>Insructor Email:</b><br/>
-                kprather@latech.edu <br/>
-                
-            </div>
-            <div>
-                <b>Instructor Phone Number:</b><br/>
-                (225) 998-2077 <br/>
-                
+            {/* General Information */}
+            <div className="flex rounded-2xl m-auto justify-center w-[50%] bg-gray-200 p-5">
+                {/* Left Info */}
+                {/* Description and schedule */}
+                <div className="grid text-left px-2 w-[50%]">
+                    <div className="text-2xl">
+                        <b>Course Name:</b><br/>
+                        CSC 4052
+                    </div>
+                    <div className="">
+                        <b>Course Desc:</b><br/>
+                        Senior Capstone I is the first of two courses that compose the Computer Science Senior Design series. Upon completion of 
+                        this course, students and their teams will continue development on their current projects 
+                        into Senior Capston II. <br/>
+                        
+                    </div>
+                    <div>
+                        <b>Course Schedule:</b><br/>
+                        MWF 10 am - 11:15 am <br/>
+                    </div>
+                </div>
+
+                {/* Right, Stacked Info */}
+                <div className="grid text-left px-2 w-[50%]">
+                    <div className=" text-2xl">
+                        <b>Instructor:</b><br/>
+                        Kyle Prather
+                    </div>
+                    <div className="">
+                        <b>Office hours:</b><br/>
+                        MWF 9 am - 4 pm
+                    </div>
+                    <div className="">
+                        <b>Insructor Email:</b><br/>
+                        kprather@latech.edu
+                    </div>
+                    <div className="">
+                        <b>Instructor Phone Number:</b><br/>
+                        (225) 998-2077
+                    </div>
+                </div>
             </div>
 
+            {/* Uesr-Course Resources */}
+            <div className="bg-grey-200">
+                <b className="text-xl">Course Resources:</b>
+                <div className="flex justify-center p-2">
+                    <div>
+                        Syllabus, 
+                    </div>
+                    <div>
+                        Assignment 1, 
+                    </div>
+                    <div>
+                        Assignment 2,
+                    </div>
+                </div>
+            </div>
+
+            {/* Footer */}
+            <p className="text-black font-mono font-thin text-sm mt-6">
+            More than just notes !
+            </p>
         </div>
-        <div className="flex rounded-2xl m-[20%] my-5 bg-red-500 ">
-
-            <div className="m-auto w-[10%]">
-                <b>Course Desc:</b><br/>
-                Senior Capstone I is the first of two courses that compose the Computer Science Senior Design series. Upon completion of 
-                this course, students and their teams will continue development on their current projects 
-                into Senior Capston II. <br/>
-                
-            </div>
-            <div>
-                <b>Course Schedule:</b><br/>
-                MWF 10 am - 11:15 am <br/>
-                
-            </div>
-
-        </div>
-        <div>
-            <b>Course Resources:</b><br/>
-            Syllabus, Assignment 1, Assignment 2,  <br/>
-        </div>
-
-        <p className="text-black font-mono font-thin text-sm mt-6">
-          More than just notes !
-        </p>
-
-      </div>
+    </body>
   );
 }
