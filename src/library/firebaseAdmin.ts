38f3a710bfba608c6da4f6612029da89eee8f5da
ashx004 <ashx004@gmail.com>
2026-09-25 17:50:56 -0500

    Exposing admin auth so verification links can be generated through Firebase

--- src/library/firebaseAdmin.ts
+++ src/library/firebaseAdmin.ts
@@ -1,5 +1,6 @@
 import { cert, getApps, initializeApp, } from "firebase-admin/app";
 import { getFirestore, } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
 
 const adminApp =
   getApps().length > 0
@@ -13,3 +14,4 @@ const adminApp =
       });
 
 export const adminDb = getFirestore(adminApp);
+export const adminAuth = getAuth(adminApp);
